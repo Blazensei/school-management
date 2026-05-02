ab4946bc9c9f4c8cc9fa540d2abea0b5636f42fb
Jhamnour <saripjham@gmail.com>
2026-05-02 16:05:36 +0800

    revert test

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,6 @@ const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
   : "http://localhost:3000";
 
-  
 export const metadata: Metadata = {
   metadataBase: new URL(defaultUrl),
   title: "Next.js and Supabase Starter Kit",
